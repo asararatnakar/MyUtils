8f9a8b27f641c9a1237b55b0ec45ac075bf87d05
ratnakar <rasara@us.ibm.com>
2016-07-27 15:21:20 -0400

    few more checks + time calculations

--- TransactionsCalci.sh
+++ TransactionsCalci.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-# ------------------------------------------------------------------ 
+# ------------------------------------------------------------------
 #
 # TITLE : Transaction Calculator
 #
 # AUTHOR: Ratnakar Asara
 #
-# VERSION: 0.1
+# VERSION: 0.2
 #
 # DESCRIPTION:
-#          The purpose of this script is to calculate number of 
+#          The purpose of this script is to calculate number of
 # successful transactions taken place on fabric. Script gets the
 # chain height and segragates Deploy, Failed and Total transactions
-# executed on blockchain
+# executed on blockchain, also calculates total time taken to
+# execute the transactions
 #
 # DEPENDENCY:
 #	Download and Install JQ: https://goo.gl/DsDskg
@@ -22,28 +23,31 @@
 # OPTIONS:
 #	-h/? - Print a usage message
 #	-i   - IP and HOST
-#       -b   - Block number from where calculations begins
-#       -f   - Enable Block info logging to file
+#       -s   - Starting Block number
+#       -e   - End Block number
+#       -l   - Enable Block info logging to file
 #
 # SAMPLE :
-#	./TransactionsCalci.sh -i http://127.0.0.1:5000 -b 2 -f
-#       Transaction infomration is calculates from block 2 and block
+#	./TransactionsCalci.sh -i http://127.0.0.1:5000 -s 2 -l
+#       Transaction infomration is calculated from block 2 and block
 # info will be saved to blocks.txt
 # ------------------------------------------------------------------
 
 function usage(){
 	## Enhance this section
-        echo "USAGE : TransactionsCalci.sh -i http://IP:PORT -b <BLOCK_NUMBER_FROM> -f"
-	echo "ex: ./TransactionsCalci.sh -i http://127.0.0.1:5000 -b 2 -f"
+        echo "USAGE : TransactionsCalci.sh -i http://IP:PORT -s <START_BLOCK_NUM> -e <END_BLOCK_NUM> -l"
+	echo "ex: ./TransactionsCalci.sh -i http://127.0.0.1:5000 -b 2 -l"
 }
 
-while getopts "\?hefi:b:" opt; do
+while getopts "\?hli:s:e:" opt; do
   case $opt in
+     s)   START_BLOCK_NUM="$OPTARG"
+        ;;
+     e)   END_BLOCK_NUM="$OPTARG"
+        ;;
      i)   IP_PORT="$OPTARG"
 	;;
-     f)   ENABLE_LOG="Y"
-	;;
-     b)   BLOCK_NUM="$OPTARG"
+     l)   ENABLE_LOG="Y"
 	;;
    \?|h)  usage
           exit 1
@@ -53,25 +57,51 @@ done
 
 echo
 echo "##################### Letz begin the fun  #######################"
-#Wrong way of cheking ?
-#TEMP_IP=$IP_PORT
 
-: ${IP_PORT:="http://127.0.0.1:5000"}
-: ${BLOCK_NUM:=1}
-: ${ENABLE_LOG:="N"}
+: ${IP_PORT:="http://127.0.0.1:5000"} #Default IP set to 127.0.0.1 and Port to 5000
+: ${START_BLOCK_NUM:=1} #Defaults to DeployTx or any First block
+: ${ENABLE_LOG:="N"} #Default logging disabled
+: ${START_TIME:="0"} #Default Start time stamp
+: ${END_TIME:="0"} #Default End time stamp
 
+#Set default values to 0
 deployTrxn=0
 errTrxn=0
 Trxn=0
 
 echo
-echo "http[s]://IP:PORT --- $IP_PORT"
 IS_SECURE=${IP_PORT:0: 5}
-echo
 if [ "$IS_SECURE" = "https" ]; then
     TOTAL_TRXNS=$(curl -k $IP_PORT/chain | jq '.height')
-else 
+else
     TOTAL_TRXNS=$(curl -s $IP_PORT/chain | jq '.height')
+fi
+
+if [ -z $TOTAL_TRXNS ]; then
+	echo
+	echo "Looks like IP and/or PORT are Invalid or May be Network is bad ??"
+	echo
+	echo "##### Oops can't help, Please reset your network and come back #####"
+	echo
+	exit 1
+else
+	echo "Chain height on $IP_PORT is $TOTAL_TRXNS"
+fi
+
+if test "$TOTAL_TRXNS" -le 1 ; then
+	echo
+	echo "... All you have got is a genesis block, no transactions available yet ..."
+	echo
+	echo "################### Exiting ###################"
+	echo
+	exit 1
+fi
+
+if [ -z $END_BLOCK_NUM ]; then
+        #comeup with a better approach?
+	END_BLOCK_NUM=$TOTAL_TRXNS
+else
+	echo "End Block number is $END_BLOCK_NUM"
 fi
 
 echo "--- Total Blocks to be processed ` expr $TOTAL_TRXNS - 1 ` (Ignore Firt Block Genesis) ---"
@@ -80,12 +110,21 @@ if [ "$ENABLE_LOG" == "Y" ] ; then
 	echo "############ Begin Writing blocks ############" > blocks.txt
 fi
 
-for (( i=$BLOCK_NUM; $i<$TOTAL_TRXNS; i++ ))
+#Calculate starting block
+if [ "$IS_SECURE" = "https" ]; then
+	curl -k $IP_PORT/chain/blocks/$START_BLOCK_NUM | jq '.' > timeCal.json
+else
+	curl -s $IP_PORT/chain/blocks/$START_BLOCK_NUM | jq '.' > timeCal.json
+fi
+
+START_TIME=$(cat timeCal.json | jq '.["nonHashData"]["localLedgerCommitTimestamp"]["seconds"]')
+
+for (( i=$START_BLOCK_NUM; $i<$END_BLOCK_NUM; i++ ))
 do
 	#This check is required
 	if [ "$IS_SECURE" = "https" ]; then
 		curl -k $IP_PORT/chain/blocks/$i | jq '.' > data.json
-	else 
+	else
 		curl -s $IP_PORT/chain/blocks/$i | jq '.' > data.json
 	fi
 
@@ -105,23 +144,53 @@ do
 	counter=$(cat data.json | jq '.["nonHashData"]["transactionResults"]' | grep errorCode | wc -l)
 	errTrxn=` expr $errTrxn + $counter `
 
-	#Calculate Error transactions
+	#Calculate overall transactions
 	counter=$(cat data.json | jq '.["nonHashData"]["transactionResults"]' | grep uuid | wc -l)
 	Trxn=` expr $Trxn + $counter `
 done
 
+#Calculate end block
 if [ "$ENABLE_LOG" == "Y" ] ; then
 	echo "############ End Writing blocks ############" >> blocks.txt
 fi
 
+#Calculate starting block
+if [ "$IS_SECURE" = "https" ]; then
+	curl -k $IP_PORT/chain/blocks/` expr $END_BLOCK_NUM - 1 ` | jq '.' > timeCal.json
+else
+	curl -s $IP_PORT/chain/blocks/` expr $END_BLOCK_NUM - 1 ` | jq '.' > timeCal.json
+fi
+
+END_TIME=$(cat timeCal.json | jq '.["nonHashData"]["localLedgerCommitTimestamp"]["seconds"]')
+
+#Temporary files cleanup
 if [ -f ./data.json ]; then
     rm ./data.json
+fi
+
+if [ -f ./timeCal.json ]; then
+    rm ./timeCal.json
+fi
+
+if test "$START_TIME" == "null" ; then
+	START_TIME=0
+fi
+if test "$END_TIME" == "null" ; then
+	END_TIME=0
 fi
 
 echo
 echo "----------- Chaincode Deployment Transactions: $deployTrxn"
 echo "----------- Failed Transactions: $errTrxn"
-echo "----------- Total Successful Transactions (Exclude Deploy Trxn) : " ` expr $Trxn - $errTrxn`
+echo "----------- Successful Transactions (Exclude Deploy Trxn) : " ` expr $Trxn - $errTrxn`
 echo
+echo
+echo "************ Total Transactions : " ` expr $deployTrxn + $Trxn + $errTrxn` " **********"
+echo
+if test -n "$START_TIME" -a -n "$END_TIME" ; then
+	echo "------------ Total execution time ` expr $END_TIME - $START_TIME ` in ms"
+fi
+echo
+
 echo "############ Thatz all I have for now, Letz catchup some other time  ###############"
 echo

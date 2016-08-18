30fd06aff97a347b07ec90f65cce6e73fd146a40
ratnakar <rasara@us.ibm.com>
2016-08-18 16:57:59 -0400

    Helper script to convert Ascii to text

---
+++ ascii2text.js
@@ -0,0 +1,22 @@
+
+if (process.argv.length < 3) {
+    console.log("\nUSAGE: node ascii2text.js <ascii>\n");
+    console.log("example: \n");
+    console.log('node helloblockachain.js "99 111 110 115 10 110 115 117"\n');
+    process.exit();
+}
+
+if (process.argv[2].length < 1 ){
+   console.log("\nInvalid ASCII value, Exiting ... Retry ...\n");
+   process.exit();
+}
+var str = process.argv[2];
+var res = str.split(" ");
+var result = '';
+
+for (var i=0;i<res.length;i++) {
+  var temp = String.fromCharCode(parseInt(res[i]));
+  result += temp;
+}
+console.log("Text now is : " +result+"\n");
+
